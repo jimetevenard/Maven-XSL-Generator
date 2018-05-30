230b14d9a3e1977a49a3fa72872f0504cd0a2c4e
Jim ETEVENARD <j.etevenard-ext@lefebvre-sarrut.eu>
2018-05-30 17:56:08 +0200

    Refacto - utilisation de la classe Driver

--- src/main/java/com/jimetevenard/xslt/generate/Main.java
+++ src/main/java/com/jimetevenard/xslt/generate/Main.java
@@ -1,10 +1,6 @@
 package com.jimetevenard.xslt.generate;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Set;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -14,13 +10,8 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 import com.jimetevenard.utils.MavenLogger;
-import com.jimetevenard.xslt.GenerationScenario;
-import com.jimetevenard.xslt.ScenariParser;
-import com.jimetevenard.xslt.implSaxon.SaxonScenariParser;
-import com.jimetevenard.xslt.implSaxon.SaxonXSLTGenerator;
-import com.jimetevenard.xslt.implSaxon.SaxonXSLTGenerator.GenerationException;
-
-import net.sf.saxon.s9api.SaxonApiException;
+import com.jimetevenard.xslt.Driver;
+import com.jimetevenard.xslt.GenerationException;
 
 @Mojo(name = "generate-xsl")
 public class Main extends AbstractMojo {
@@ -47,74 +38,39 @@ public class Main extends AbstractMojo {
 		// *******************************************************************************
 		this.getLog().info("[GENERATE XSL] does actually nothing, and is proud of it !");
 		// *******************************************************************************
-
-
-		MavenXslGenerator generator = new MavenXslGenerator(MavenLogger.of(this.getLog()),
-				this.project.getBasedir().getAbsolutePath());
-
-		File scenariFile = new File(this.project.getBasedir(), scenariXmlFilePath); 
-		ScenariParser scenariParser = ScenariParser.newInstance(scenariFile.getAbsolutePath());
-
+			
+		File catalog = new File(this.project.getBasedir(), "catalog.xml");					
+		File scenariFile = new File(this.project.getBasedir(), scenariXmlFilePath);
 		
+		Driver driver = new Driver(MavenLogger.of(this.getLog()), scenariFile, catalog);
+		
+		driver.setLicencedSaxon(true); // TODO whoooooo !!
+		
+		
+		debugDir(driver);
+			
+		// Allez, zou !
 		try {
-			scenariParser.parse();
-			Set<GenerationScenario> scenari = scenariParser.getScenari();
-
-			
-			
-			
-			/*
-			 * Les paramètre XSLT sont exprimés dans le fichier des scenari sous forme
-			 * d'une expression XPath.
-			 * 
-			 * Le Context-Item utilisé pour l'évaluation de ce XPath est la racine du fichier des scenari.
-			 * (Utile notemment pour la resolution d'URI relatives)
-			 * 
-			 *
-			 * 
-			 * TODO : Pas clair du tout, Gros refacto dans Generate-xsl.
-			 * 
-
-			 */
-			((SaxonXSLTGenerator) generator.getXslGenerator()).getSaxonCompiler()
-					.setParamsContextItem(((SaxonScenariParser) scenariParser).getScenariFileNode());
-
-			File intermediateXslDir = intermediateDir();
-			for (GenerationScenario s : scenari) {
-				if(intermediateXslDir == null){
-					generator.getXslGenerator().compile(s.getParams(), s.getSourceXslPath(), s.getTargetXslPath());
-				} else {
-					String intermediateFilePath = new File(intermediateXslDir, intermediateFileName()).getAbsolutePath();
-					generator.getXslGenerator().compile(s.getParams(), s.getSourceXslPath(), s.getTargetXslPath(), intermediateFilePath);
-				}
-			}
-		} catch (SaxonApiException | URISyntaxException | GenerationException e) {
-			// TODO Auto-generated catch block
-			throw new MojoExecutionException("Oupsy !", e);
-		}
-
-	}
-	
-	private String intermediateFileName(){
-		return "intermediate-" + System.currentTimeMillis()
-		+ '-' + Math.round(Math.random() * 10000) + ".xsl";
-	}
-	
-	private File intermediateDir(){
-		if(intermediateXslDirPath != null && !(intermediateXslDirPath.isEmpty())){
-			File dir = new File(intermediateXslDirPath);
-			if(dir.isDirectory()){
-				return dir;
-			} else {
-				getLog().error(
-						intermediateXslDirPath
-						+ " is not an existing directory. intermediate XSL won't be saved."
-					);
-			}
+			driver.launch();
+		} catch (GenerationException e) {
+			throw new MojoFailureException("Oupsy !", e);
 		}
 		
-		return null;
+
 	}
+	
+	private void debugDir(Driver driver){
+		if(intermediateXslDirPath != null){
+			File debugDir = new File(intermediateXslDirPath);
+			if(debugDir.exists()){
+				driver.setDirForIntermediates(debugDir);
+			} else {
+				this.getLog().warn(debugDir.getAbsolutePath() + "is not an existing directory. intermediate XSLs will not be stored.");
+			}
+		} 
+	}
+	
+
 
 
 }
